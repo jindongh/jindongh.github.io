c0e87b850b9933531faf7403cf54bf9b5a4f27d3
hankjin <jindongh@gmail.com>
2014-06-26 16:59:57 +0800

    fix code highlight

--- js/default.js
+++ js/default.js
@@ -1,8 +1,2 @@
 var bodies = document.getElementsByTagName("body");
 bodies[0].setAttribute("onload", "prettyPrint()");
-
-var pres = document.getElementsByTagName("pre");
-for (var i = 0; i < pres.length; i++)
-{
-        pres[i].setAttribute("class", "prettyprint linenums");
-}
