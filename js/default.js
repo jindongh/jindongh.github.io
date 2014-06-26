3e480465b8c95b667eb0b60e796c32efe32e59aa
hankjin <jindongh@gmail.com>
2014-06-26 10:53:24 +0800

    upgrade prettify and bug fix of <<

--- js/default.js
+++ js/default.js
@@ -1,7 +1,7 @@
 var bodies = document.getElementsByTagName("body");
 bodies[0].setAttribute("onload", "prettyPrint()");
 
-var pres = document.getElementsByTagName("pre");
+var pres = document.getElementsByTagName("code");
 for (var i = 0; i < pres.length; i++)
 {
         pres[i].setAttribute("class", "prettyprint linenums");
